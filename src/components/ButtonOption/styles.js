199d2f9c2384a7c0d22675de51e6837c7ecb3453
luizmigueldev <luizmigueln.dev@gmail.com>
2021-08-05 13:50:51 -0300

    Realçando botão do item selecionado

--- src/components/ButtonOption/styles.js
+++ src/components/ButtonOption/styles.js
@@ -6,8 +6,7 @@ export const Button = styled.button`
   width : 120px;
   padding: 8px;
   border-radius: 4px;
-  border-color: ${({ selected }) => selected && "#55FF5D"};
   color : #a5a5a5;
-  border : 2px solid #a5a5a5;
+  border : 2px solid ${({ selected }) => selected ? "#2D9C17" : "#a5a5a5"};
   margin: 10px;
 `;
--- src/pages/Order/FlavorAndSize/index.js
+++ src/pages/Order/FlavorAndSize/index.js
@@ -1,23 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ButtonOption from "../../../components/ButtonOption"
 import ButtonAction from "../../../components/ButtonAction"
 import { Content } from './styles'
 
 export default function FlavorAndSize({ flavors, sizes, selectFlavor, selectSize, nextStep }) {
+
+  const [selectedFlavor, setSelectedFlavor] = useState()
+  const [selectedSize, setSelectedSize] = useState()
+
+  function handleSelectFlavor(flavor) {
+    setSelectedFlavor(flavor);
+    selectFlavor(flavor)
+  }
+
+  function handleSelectSize(size) {
+    setSelectedSize(size);
+    selectSize(size)
+  }
+
   return (
     <Content>
       <h1>Escolha seu açai</h1>
       <h2>SABOR:</h2>
       <div>
         {
-          flavors && flavors.map(flavor => <ButtonOption title={flavor} />
+          flavors && flavors.map(flavor => <ButtonOption
+            onClick={() => {
+              handleSelectFlavor(flavor)
+            }}
+            title={flavor}
+            selected={flavor == selectedFlavor} />
           )
         }
       </div>
       <h2>TAMANHO:</h2>
       <div>
         {
-          sizes && sizes.map(size => <ButtonOption title={size} />
+          sizes && sizes.map(size => <ButtonOption
+            onClick={() => {
+              handleSelectSize(size)
+            }}
+            title={size}
+            selected={size == selectedSize} />
           )
         }
       </div>
